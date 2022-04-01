73d70b35ac090491daddc21db25187cf069fdbb5
SirJoc <josiasolaya2016@gmail.com>
2022-03-31 21:08:46 -0500

    update

--- SolutionFront/src/app/pages/cloth/cloth.component.ts
+++ SolutionFront/src/app/pages/cloth/cloth.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatSort} from "@angular/material/sort";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatTableDataSource} from "@angular/material/table";
+import {NgForm} from "@angular/forms";
+import {ClothsApiService} from "../../services/cloth-api.service";
+import {Router} from "@angular/router";
+import {Cloth} from "../../models/cloth";
+// @ts-ignore
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-cloth',
@@ -6,10 +15,124 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cloth.component.css']
 })
 export class ClothComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'marca', 'color', 'actions'];
+  clothData: Cloth;
+  dataSource = new MatTableDataSource();
+  isFiltering = false;
+  @ViewChild('clothForm', { static: false }) clothForm!: NgForm;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  constructor() { }
+  imagen_src = "";
+  isInfoMode = false;
+  isEditMode = false;
+  isCreateMode = true;
+  c_cloths = [
+    {value: 'Rosa', viewValue: "Rosa", type: "jpeg"},
+    {value: 'Azul', viewValue: "Azul", type: "jpg"},
+    {value: 'Verde', viewValue: "Verde", type: "jpeg"}
+  ];
 
-  ngOnInit(): void {
+
+
+  constructor(private clothsApi: ClothsApiService, private router: Router) {
+    this.clothData = {} as Cloth;
   }
 
+  ngOnInit(): void {
+    this.getAllCloth();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+    this.isFiltering = !!filterValue;
+  }
+
+  getAllCloth(): void {
+    this.clothsApi.getAllCloths().subscribe((response: any) => {
+      this.dataSource.data = response;
+      console.log(this.dataSource.data);
+    })
+  }
+
+  addCloth(): void {
+    const newCloth = {brand: this.clothData.brand, color: this.clothData.color};
+
+    this.clothsApi.addCloth(newCloth).subscribe((response: any) => {
+      this.dataSource.data.push({...response});
+      this.dataSource.data = this.dataSource.data.map(o => o);
+    });
+  }
+
+  deleteItem(id: number): void {
+    this.clothsApi.deleteCloth(id).subscribe(() => {
+      this.dataSource.data = this.dataSource.data.filter((o: any) => {
+        return o.id !== id ? o : false;
+      });
+    });
+    console.log(this.dataSource.data);
+  }
+
+  updateCloth(): void {
+    this.clothsApi.updateCloth(this.clothData.id, this.clothData)
+      .subscribe((response: Cloth) => {
+        this.dataSource.data = this.dataSource.data.map((o: any) => {
+          if (o.id === response.id) {
+            o = response;
+          }
+          return o;
+        });
+        this.cancelEdit();
+      });
+  }
+
+  editItem(element: any): void {
+    console.log(element);
+    this.clothData = _.cloneDeep(element);
+    this.isEditMode = true;
+  }
+
+  cancelEdit(): void {
+    this.isEditMode = false;
+    this.isInfoMode = false;
+    this.clothForm.resetForm();
+  }
+
+  onSubmit(): void {
+    if (this.clothForm.form.valid) {
+      if (this.isEditMode) {
+        this.updateCloth();
+      } else {
+        this.addCloth();
+      }
+    } else {
+      console.log('Invalid Data');
+    }
+  }
+
+  infoItem(element: any): void {
+    console.log(element);
+    this.clothData = _.cloneDeep(element);
+    this.isInfoMode = true;
+    if (element.clothData.color == "Rosa"){
+      this.imagen_src="/assets/cloths/rosa.jpeg";
+    }else if (element.clothData.color == "Azul"){
+      this.imagen_src="/assets/cloths/azul.jpg";
+    }else if (element.clothData.color == "Verde"){
+      this.imagen_src="/assets/cloths/verde.jpeg";
+    }else{
+      this.isInfoMode = false;
+    }
+  }
 }
+
+
--- SolutionFront/src/app/pages/home/home.component.ts
+++ SolutionFront/src/app/pages/home/home.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material/dialog";
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,10 +12,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  animal: string = "";
+  name: string = "";
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
+
   }
 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogNoMessage, {
+      width: '250px',
+      data: {name: this.name, animal: this.animal},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+}
+
+
+@Component({
+  selector: 'dialog/dialog-no-message',
+  templateUrl: 'dialog/dialog-no-message.html',
+})
+export class DialogNoMessage {
+  constructor(
+    public dialogRef: MatDialogRef<DialogNoMessage>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
